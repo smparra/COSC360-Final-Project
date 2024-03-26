ae23fac47a42550e6826e1f16ad471d7e4212c7f
Claire Costello <claire.cost.31@gmail.com>
2024-03-26 10:43:07 -0700

    quick fix on image.php

--- code/php/image.php
+++ code/php/image.php
@@ -1,16 +1,17 @@
+<!--- fetches images to display products on home page --->
 <?php
 include("configure.php");
-
 $conn =  mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
 
 if (mysqli_connect_errno()) {
   die("Connection failed: " . mysqli_connect_error());  
 }
 $id = $_GET['id'];
-$sql = "SELECT 'Image' FROM 'Products' WHERE 'ProductID'='$id'"; 
+$sql = "SELECT `Image` FROM `Products` WHERE `ProductID`='$id'"; 
+$result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();    
+    $row = $result->fetch_assoc();
     header("Content-Type: image/jpeg"); 
     echo $row['Image'];
 } else {
