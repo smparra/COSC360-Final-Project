d559bf28922cd399b5ed559517264bfb7553bb48
Segundo Parra J <49846907+smparra@users.noreply.github.com>
2024-03-26 14:09:45 -0700

    working db

--- code/configure.php
+++ code/configure.php
@@ -1,6 +1,10 @@
 <?php
 define('DBHOST', 'localhost'); # cosc360.ok.ubc.ca
-define('DBNAME', 'dbl_10300200'); # db_10300200
-define('DBUSER', 'root'); # 10300200
-define('DBPASS', 'password'); # 10300200
+define('DBNAME', 'db_44934693'); # db_10300200
+define('DBUSER', '44934693'); # 10300200
+define('DBPASS', '44934693'); # 10300200
+#define('DBHOST', 'localhost'); # cosc360.ok.ubc.ca
+#define('DBNAME', 'dbl_10300200'); # db_10300200
+#define('DBUSER', 'root'); # 10300200
+#define('DBPASS', 'password'); # 10300200
 ?>
