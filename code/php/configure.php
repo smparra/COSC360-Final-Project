17c19ac4365ce745787102b485124778364a22b1
Claire Costello <claire.cost.31@gmail.com>
2024-03-28 14:00:37 -0700

    Update configure.php
    updated to match cosc360.ok.ubc.ca credentials

--- code/php/configure.php
+++ code/php/configure.php
@@ -1,6 +1,6 @@
 <?php
-define('DBHOST', 'localhost'); # cosc360.ok.ubc.ca
-define('DBNAME', 'dbl_10300200'); # db_10300200
-define('DBUSER', 'root'); # 10300200
-define('DBPASS', 'password'); # 10300200
+define('DBHOST', 'cosc360.ok.ubc.ca'); 
+define('DBNAME', 'db_10300200'); 
+define('DBUSER', '10300200'); 
+define('DBPASS', '10300200'); 
 ?>
